4e90e1483982984e9f659b50443d350d6ef704c0
Insomn1ac <emelyanoff94@gmail.com>
2022-01-20 00:11:22 +0300

    94. Binary Tree Inorder Traversal

---
+++ src/test/java/binary/tree/inorder/traversal/SolutionTest.java
@@ -0,0 +1,37 @@
+package binary.tree.inorder.traversal;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void whenRootIsNullThenEmptyList() {
+        TreeNode root = new TreeNode();
+        Solution sol = new Solution();
+        List<Integer> expected = List.of(0);
+        List<Integer> rsl = sol.inorderTraversal(root);
+        assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenTreeIs1Null23Then123() {
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        Solution sol = new Solution();
+        List<Integer> expected = List.of(1, 3, 2);
+        List<Integer> rsl = sol.inorderTraversal(root);
+        assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenTreeIs1Then1() {
+        TreeNode root = new TreeNode(1);
+        Solution sol = new Solution();
+        List<Integer> expected = List.of(1);
+        List<Integer> rsl = sol.inorderTraversal(root);
+        assertEquals(expected, rsl);
+    }
+}
